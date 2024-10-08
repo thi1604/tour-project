283938b7fd0efec3ad96e63aa12b24f28af5dd5d
thi1604 <thibeo1604@gmail.com>
2024-10-08 17:14:22 +0700

    Lay tour theo slug(chua sua giao dien)

--- controllers/client/tours.controller.ts
+++ controllers/client/tours.controller.ts
@@ -1,5 +1,42 @@
 import {Request, Response} from "express";
+import { QueryTypes } from "sequelize";
+import sequelize from "../../config/database";
 
 export const index = async(req:Request, res: Response) => {
-  res.render("client/pages/tour/index.pug");
+  
+  const slug = req.params.slugTour;
+  
+  const listTours = await sequelize.query(`
+    SELECT tours.*, ROUND(price * (1 - discount/100)) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE
+      categories.slug = '${slug}'
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active'; 
+    `, {
+      type: QueryTypes.SELECT
+    }
+  );
+
+  for(const item of listTours){
+    if(item["images"]){
+      const listImage = JSON.parse(item["images"]);
+      if(listImage.length > 0){
+        item["image"] = listImage[0];
+      }
+    }
+    item["price_special"] = parseInt(item["price_special"]);
+  }
+
+  console.log(listTours);
+
+  res.render("client/pages/tour/index.pug", {
+    pageTitle: `Danh sách tours theo danh mục`,
+    tours: listTours
+  });
+  // res.send("ok");
 };
--- routes/client/tours.route.ts
+++ routes/client/tours.route.ts
@@ -3,5 +3,6 @@ import * as controller from "../../controllers/client/tours.controller";
 const route = express.Router();
 
 route.get("/", controller.index);
+route.get("/detail/:slugTour", controller.index);
 
 export const routeTours = route;
